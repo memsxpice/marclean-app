4a4dd10afeb01735b751abfbc7193da51c344954
GUILLERMO OCHOA JIMENEZ <Guillermo.Ochoa@ibm.com>
2024-10-01 19:58:52 -0600

    MC-001 Tarea 2 Se dejo en blanco la pagina para poder incluir los productos de venta

--- src/Components/PuntoVenta/PuntoVenta.jsx
+++ src/Components/PuntoVenta/PuntoVenta.jsx
@@ -4,22 +4,7 @@ import './PuntoVenta.css';
 const PuntoVenta = () => {
     return (
         <div className='container'>
-            <div className='header'>
-                <div className='textTitle'>Login Punto de Venta</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <input type="text" placeholder='Usuario'/>
-                </div>
-                <div className="input">
-                    <input type="password" placeholder='Contraseña'/>
-                </div>
-            </div>
-            <div className="forgot-password">¿Olvidaste tu contraseña? <span>Clik aqui</span></div>
-            <div className="submit-container">
-                <div className="submit">Iniciar Sesion</div>
-            </div>
+            
         </div>
     )
 }
