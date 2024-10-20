176dc700b79ec518f34f9c7aacfcdfa40f7bd8af
Davvapor1 <178967659+Davvapor1@users.noreply.github.com>
2024-10-20 14:13:58 -0600

    Update PuntoVenta.jsx
    editado el 20-10-2024 para Proyecto II.
    
    Se agrego un titulo

--- src/Components/PuntoVenta/PuntoVenta.jsx
+++ src/Components/PuntoVenta/PuntoVenta.jsx
@@ -4,6 +4,10 @@ import './PuntoVenta.css';
 const PuntoVenta = () => {
     return (
         <div className='container'>
+
+<h1> Articulos de limpieza  </h1>
+
+
             
         </div>
     )
