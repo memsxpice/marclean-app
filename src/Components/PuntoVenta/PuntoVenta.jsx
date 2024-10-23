d47abde5add2d322a687f412dde9369b02672e25
GUILLERMO OCHOA JIMENEZ <Guillermo.Ochoa@ibm.com>
2024-10-22 19:07:45 -0600

    Defecto-001 Se cambio el metodo de referenciar imagenes en React

--- src/Components/PuntoVenta/PuntoVenta.jsx
+++ src/Components/PuntoVenta/PuntoVenta.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './PuntoVenta.css';
+import jabonPolvo from '../Assets/jabonPolvo.png';
+import jabonLiquido from '../Assets/jabonLiquido.png';
 
 const PuntoVenta = () => {
     return (
         <div className='container'>
-
-    <img src="limpieza dos.jpg" alt="">
-
-    <img src="limpieza.png" alt="">
-
+            <h1> Articulos de limpieza  </h1>
+            <img src={jabonPolvo} alt="jabonPolvo"/>
+            <img src={jabonLiquido} alt="jabonLiquido"/>
             
         </div>
     )
