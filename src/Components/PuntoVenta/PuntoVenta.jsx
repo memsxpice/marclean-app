444c9fb05e1d18314ac23f7271c669b9f782d490
GUILLERMO OCHOA JIMENEZ <Guillermo.Ochoa@ibm.com>
2024-10-23 20:44:20 -0600

    MC-003 Tarea 1 Se crearon los botones para agregar articulos a la calculadora

--- src/Components/PuntoVenta/PuntoVenta.jsx
+++ src/Components/PuntoVenta/PuntoVenta.jsx
@@ -2,14 +2,51 @@ import React from 'react';
 import './PuntoVenta.css';
 import jabonPolvo from '../Assets/jabonPolvo.png';
 import jabonLiquido from '../Assets/jabonLiquido.png';
+import jabonBarra from '../Assets/jabonBarra.png';
+import jabonAuto from '../Assets/jabonAuto.png';
+import jabonManos from '../Assets/jabonManos.png';
 
 const PuntoVenta = () => {
     return (
         <div className='container'>
-            <h1> Articulos de limpieza  </h1>
-            <img src={jabonPolvo} alt="jabonPolvo"/>
-            <img src={jabonLiquido} alt="jabonLiquido"/>
-            
+            <h1> Punto de venta  </h1>
+            <table>
+                <tr>
+                    <th>Producto</th>
+                    <th>Descripcion</th>
+                    <th>Precio</th>
+                </tr>
+                <tr>
+                    <td><img src={jabonPolvo} alt="jabonPolvo" width=" 100" height="100"/></td>
+                    <td>Jabon en Polvo Roma</td>
+                    <td>$75.00</td>
+                    <td> <button type="button">Agregar</button> </td>
+                </tr>
+                <tr>
+                    <td><img src={jabonLiquido} alt="jabonLiquido" width="100" height="100"/></td>
+                    <td>Jabon Liquido Axion</td>
+                    <td>$60.00</td>
+                    <td> <button type="button">Agregar</button> </td>
+                </tr>
+                <tr>
+                    <td><img src={jabonBarra} alt="jabonBarra" width="100" height="100"/></td>
+                    <td>Jabon Zote</td>
+                    <td>$33.00</td>
+                    <td> <button type="button">Agregar</button> </td>
+                </tr>
+                <tr>
+                    <td><img src={jabonAuto} alt="jabonAuto" width="100" height="100"/></td>
+                    <td>Jabon para Auto</td>
+                    <td>$140.00</td>
+                    <td> <button type="button">Agregar</button> </td>
+                </tr>
+                <tr>
+                    <td><img src={jabonManos} alt="jabonManos" width="100" height="100"/></td>
+                    <td>Jabon de Manos</td>
+                    <td>$27.00</td>
+                    <td> <button type="button">Agregar</button> </td>
+                </tr>
+            </table>
         </div>
     )
 }
